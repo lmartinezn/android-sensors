53e3866765b7e86c9335a8f11410987220c39d94
lmartinezn <lmartinezn@edu.tecnocampus.cat>
2019-05-15 09:33:14 +0200

    Implemented SensorEventListener

--- app/src/main/java/cat/tecnocampus/mobileapps/practica1/lightsensortest/MainActivity.java
+++ app/src/main/java/cat/tecnocampus/mobileapps/practica1/lightsensortest/MainActivity.java
@@ -2,13 +2,15 @@ package cat.tecnocampus.mobileapps.practica1.lightsensortest;
 
 import android.content.Context;
 import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
     private TextView textValues;
     private SensorManager mSensorManager;
     private Sensor lightSensor;
@@ -37,5 +39,23 @@ public class MainActivity extends AppCompatActivity {
                     lightSensor,
                     SensorManager.SENSOR_DELAY_NORMAL); //Delay, cada quant rebrem valors
         }
+    }
+
+    @Override
+    protected void onPause() {
+        mSensorManager.unregisterListener(this); //Ens de-registrem abans de pausar la nostra app
+        super.onPause();
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        float lux = sensorEvent.values[0];
+        String oldValue = textValues.getText().toString();
+        textValues.setText(lux + "\n" + oldValue );
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
     }
 }
